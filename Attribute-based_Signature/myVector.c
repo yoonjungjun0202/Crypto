497c82622bd3da0a5f95bc0d51dac938b1a1ed33
yoonjungjun0202 <love667@naver.com>
2017-11-25 16:33:01 -0500

    Add element_get_intersection_vector_Zr function.

--- Attribute-based_Signature/myVector.c
+++ Attribute-based_Signature/myVector.c
@@ -88,6 +88,41 @@ void element_copy_vector(vecter_t _vo, vecter_t _vi)
 void element_clear_vector(vecter_t _v)
 {   
 	int i;
-	for(i=0; i<_v->size; i++)
+	for(i=0; i<_v->size; ++i)
 		element_clear(_v->val[i]);
 } 
+
+/*
+ * Get intersection between v0 and v1.
+ */
+vecter_ptr element_get_intersection_vector_Zr(pairing_t _pairing, vecter_t _v0, vecter_t _v1)
+{
+	int i, j, pos=0, tmpCnt = (_v0->size < _v1->size) ? (_v0->size) : (_v1->size);
+	vecter_ptr intersection;
+	vecter_t tmp;
+
+	element_init_vector_Zr(_pairing, tmp, tmpCnt);
+	for(i=0; i<_v0->size; ++i)
+	{
+		for(j=0; j<_v1->size; ++j)
+		{
+			if(0 == element_cmp(_v0->val[i], _v1->val[j]))
+			{
+				element_set(tmp->val[pos], _v0->val[i]);
+				++pos;
+				break;
+			}
+		}
+	}
+
+	if(0 == pos)
+		return NULL;
+
+	intersection = (vecter_ptr) malloc (sizeof(struct vecter_s));
+	element_init_vector_Zr(_pairing, intersection, pos);
+	element_copy_vector(intersection, tmp);
+
+	element_clear_vector(tmp);
+
+	return intersection;
+}
--- Attribute-based_Signature/myVector.h
+++ Attribute-based_Signature/myVector.h
@@ -24,6 +24,7 @@ void element_random_vector(vecter_t _v);
 void element_get_random_in_vector(vecter_t _vo, vecter_t _vi, int _cnt);
 void element_copy_vector(vecter_t _vo, vecter_t _vi);
 void element_clear_vector(vecter_t _v);
+vecter_ptr element_get_intersection_vector_Zr(pairing_t _pairing, vecter_t _v0, vecter_t _v1);
 /* End of defined function. */
 
 #endif
